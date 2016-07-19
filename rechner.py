adda4f1cf8b05bfa7ba8d9eb563b6582f8afa363
Sven Hergenhahn <sven.hergenhahn@tis.biz>
2016-07-19 10:01:01 +0200

    Refactored into main class

--- rechner.py
+++ rechner.py
@@ -4,6 +4,53 @@ import sys
 from math import sqrt
 from itertools import count, islice
 
+class Rechenspiel(object):
+
+    def __init__(self, aufgaben):
+        self.alle = 0
+        self.correct = 0
+        self.seconds = 0
+        operations = [ Addition, Substraction, Division, Multiplication ]
+        op_map = [random.choice(operations) for i in range(aufgaben)]
+
+        for op in op_map:
+            r = op()
+            try:
+                self.seconds += r.seconds
+                self.correct += r.counter
+                self.alle += 1
+            except AttributeError:
+                break
+
+        self.end()
+
+    def end(self):
+        print('='*80)
+        print('Aufgaben insgesamt: %d\ndavon korrekt: %d\n\nDu hast insgesamt %d Sekunden gerechnet\n' % 
+              (self.alle,
+               self.correct,
+               self.seconds)
+             )
+
+        try:
+            ratio = int(self.correct*100/self.alle)
+        except:
+            ratio = 0
+
+        if self.alle == 0:
+            print('Du hast keine Aufgaben gerechnet.')
+        elif ratio < 50:
+            print('Hmm. Weniger als die Hälfte war richtig. Bleib dran, dann wird es bald besser.')
+        elif ratio < 70:
+            print('Gut. Deutlich mehr als die Hälfte. Bleib dran, Du kannst Dich noch steigern.')
+        elif ratio < 90:
+            print('Sehr gut. Du hast die meisten Aufgaben richtig berechnet.')
+        elif ratio < 99:
+            print('Ahhh, fast. Beinahe hättest Du alle Aufgaben richtig gehabt.  Super!')
+        else:
+            print('Perfekt! Alles richtig. Anscheinend bist Du bereits ein Mathe-As.')
+        sys.exit(0)
+
 class Operation(object):
 
     maps = { '+' : 'ADDITION',
@@ -27,7 +74,7 @@ class Operation(object):
         now = time()
         print('='*80)
         while True:
-            ui = input('\n%d %s %d = ' % (self.op1, self.opsymbol, self.op2))
+            ui = input('\nWieviel ist\n\n%d %s %d = ' % (self.op1, self.opsymbol, self.op2))
             if ui == '':
                 return None
             try:
@@ -38,11 +85,11 @@ class Operation(object):
             break
         seconds = int(time() - now)
         if ui == self.result:
-            print( "Sehr gut. Richtig!\n\n %d %s %d = %d\n\nDu hast %d Sekunden für die Berechnung gebraucht" %
+            print( "Sehr gut. Richtig!\n\n%d %s %d = %d\n\nDu hast %d Sekunden für die Berechnung gebraucht" %
                   (self.op1, self.opsymbol, self.op2, self.result, seconds))
             self.counter = 1
         else:
-            print( "Leider falsch: \n\n %d %s %d = %d\n\nDu hast %d Sekunden benötigt." %
+            print( "Leider falsch: \n\n%d %s %d = %d\n\nDu hast %d Sekunden benötigt." %
                   (self.op1, self.opsymbol, self.op2, self.result, seconds))
             self.counter = 0
         self.seconds = seconds
@@ -94,58 +141,17 @@ class Division(Operation):
         self.result = self.op1 / self.op2
         self.prompt_user()
 
-def end(alle, correct, seconds):
-    print('='*80)
-    print('Aufgaben insgesamt: %d\ndavon korrekt: %d\n\nDu hast insgesamt %d Sekunden gerechnet\n' % 
-          (alle,
-           correct,
-           seconds)
-         )
-
-    try:
-        ratio = int(correct*100/alle)
-    except:
-        ratio = 0
-
-    if alle == 0:
-        print('Du hast keine Aufgaben gerechnet.')
-    elif ratio < 50:
-        print('Hmm. Weniger als die Hälfte war richtig. Bleib dran, dann wird es bald besser.')
-    elif ratio < 70:
-        print('Gut. Deutlich mehr als die Hälfte. Bleib dran, Du kannst Dich noch steigern.')
-    elif ratio < 90:
-        print('Sehr gut. Du hast die meisten Aufgaben richtig berechnet.')
-    elif ratio < 99:
-        print('Ahhh, fast. Beinahe hättest Du alle Aufgaben richtig gehabt.  Super!')
-    else:
-        print('Perfekt! Alles richtig. Anscheinend bist Du bereits ein Mathe-As.')
-    sys.exit(0)
 
 if __name__ == "__main__":
-    alle = 0
-    correct = 0
-    seconds = 0
-    print('Willkommen beim Mathe Spiel')
-    try:
-        aufgaben = int(input('Wie viele Aufgaben möchtest Du lösen? [10] > '))
-    except:
-        aufgaben = 10
-    print('OK, Du möchtest %d Aufgaben lösen.' % aufgaben)
-    print('Du kannst das Spiel aber jederzeit mit [RETURN] beenden.\n')
-    print('Fangen wir an...')
-    operations = [ Addition, Substraction, Division, Multiplication ]
-    op_map = [random.choice(operations) for i in range(aufgaben)]
-
-    for op in op_map:
-        r = op()
+        print('Willkommen beim Mathe Spiel')
         try:
-            seconds += r.seconds
-            correct += r.counter
-            alle += 1
-        except AttributeError:
-            break
-
-    end(alle, correct, seconds)
+            aufgaben = int(input('Wie viele Aufgaben möchtest Du lösen? [10] > '))
+        except:
+            aufgaben = 10
+        print('OK, Du möchtest %d Aufgaben lösen.' % aufgaben)
+        print('Du kannst das Spiel aber jederzeit mit [RETURN] beenden.\n')
+        print('Fangen wir an...')
+        Rechenspiel(aufgaben)
 
 
 
